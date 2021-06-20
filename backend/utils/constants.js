db64ef527ea2b5013908210fe4a0b43b52f7afbf
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-06-20 22:19:41 +0300

    исправить constants.js поиенять строки на числа

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,8 +1,8 @@
 const { NODE_ENV = 'development' } = process.env;
-const { PORT = 3000 } = process.env;
+const { PORT = 3000 } = +process.env.PORT;
 const { JWT_SECRET } = (NODE_ENV !== 'production') ? { JWT_SECRET: 'super-strong-secret' } : process.env;
 const { MONGODB_URI = 'mongodb://localhost:27017/mestodb' } = process.env;
-const { SALT_ROUNDS = 10 } = process.env;
+const { SALT_ROUNDS = 10 } = +process.env.SALT_ROUNDS;
 
 module.exports = {
   PORT,
