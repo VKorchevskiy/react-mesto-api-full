26fd815dd04aa05b79b1b8da80ff345f856eab08
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-08-25 22:19:30 +0300

    fix: PORT

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,10 +1,11 @@
-// const { NODE_ENV = 'development' } = process.env;
-const { PORT = 3000 } = +process.env.PORT;
+const { NODE_ENV = 'development' } = process.env;
+const PORT = +process.env.PORT || 3000;
 const { JWT_SECRET = 'super-strong-secret' } = process.env;
 const { MONGODB_URI = 'mongodb://localhost:27017/mestodb' } = process.env;
 const { SALT_ROUNDS = 10 } = +process.env.SALT_ROUNDS;
 
 module.exports = {
+  NODE_ENV,
   PORT,
   JWT_SECRET,
   MONGODB_URI,
